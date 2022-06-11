77e44cdf1883d68fd07f360085a7b04c646caf70
NkLORd <namankotak@gmail.com>
2022-06-11 20:47:55 +0530

    naman part 11 and 12

--- src/com/company/AssetSetter.java
+++ src/com/company/AssetSetter.java
@@ -25,7 +25,7 @@ public class AssetSetter {
 
         gp.obj[3] = new obj_Door(gp);
         gp.obj[3].worldX = 10 * gp.tilesize;
-        gp.obj[3].worldY = 11 * gp.tilesize;
+        gp.obj[3].worldY = 12 * gp.tilesize;
 
         gp.obj[4] = new obj_Door(gp);
         gp.obj[4].worldX = 8 * gp.tilesize;
@@ -33,11 +33,11 @@ public class AssetSetter {
 
         gp.obj[5] = new obj_Door(gp);
         gp.obj[5].worldX = 12 * gp.tilesize;
-        gp.obj[5].worldY = 22 * gp.tilesize;
+        gp.obj[5].worldY = 23 * gp.tilesize;
 
         gp.obj[6] = new obj_Chest(gp);
         gp.obj[6].worldX = 10 * gp.tilesize;
-        gp.obj[6].worldY = 7 * gp.tilesize;
+        gp.obj[6].worldY = 8 * gp.tilesize;
 
         gp.obj[7] = new obj_Boots(gp);
         gp.obj[7].worldX = 37 * gp.tilesize;
